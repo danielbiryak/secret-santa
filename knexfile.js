65a68772b3407ba684a614952ad5661232d422c4
Daniel Biryak <71096115+danielbiryak@users.noreply.github.com>
2022-01-30 17:48:39 +0200

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ module.exports = {
     development: {
         client: 'sqlite3',
         connection: {
-            filename: './data/secret_santa.db3'
+            filename: './secret_santa.db3'
         },
         useNullAsDefault: true
     }
